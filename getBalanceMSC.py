0667a288c80f10903e58655d036d9bf6b8c6b96e
Faiz K <faizkhan00@gmail.com>
2014-02-03 14:33:45 -0600

    Added balance script

---
+++ getBalanceMSC.py
@@ -0,0 +1,23 @@
+#get balance
+import sys
+import json
+
+if sys.argv[1] == '-h' or sys.argv[1] == '--help': 
+    print "\nUsage: cat listOfAddresses.json | getBalanceMSC.py\nRequires a fully-synced omniwallet node"
+    exit()
+
+tmp='/tmp/omniwallet/addr/'    
+
+JSON = sys.stdin.readlines()
+
+listAddresses = json.loads(str(''.join(JSON)))
+results = []
+for addr in listAddresses['addresses']:
+    try:
+        f=open(tmp + addr + '.json')
+        address = json.loads(f.readline())
+        results.append({ 'address': address['address'], 'balance': address['balance']})
+    except IOError:
+        print "No such MSC address " + addr
+
+print json.dumps(results)
